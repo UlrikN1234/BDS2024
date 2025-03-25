453d0d4ca00637029f84a809b6f0bbde14f21843
UlrikN1234 <ullepwner1@gmail.com>
2025-03-25 15:02:07 +0100

    Create scheduler.py

---
+++ scheduler.py
@@ -0,0 +1,13 @@
+import schedule
+import time
+import subprocess
+
+def run_prediction():
+    subprocess.run(["python", "/app/scripts/prediction-api-script.py"])
+
+# Schedule the task to run every day at specific times
+schedule.every().day.at("07:30").do(run_prediction)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
